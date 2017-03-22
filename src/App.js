8b0189b5628b7fe7ba151336e6bb77095cf29fc5
Waihon Yew <yewwaihon@gmail.com>
2017-03-22 14:44:49 +0800

    Basics in React

--- src/App.js
+++ src/App.js
@@ -20,24 +20,115 @@ const list = [
   }
 ]
 
+// Higher order function - A function which returns a function
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // Shorthand property syntax - when the state and var have the same name
+      list,
+      searchTerm: ""
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList })
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
+    // ES6 destructuring
+    const { searchTerm, list } = this.state;
     return (
       // A concise body wihout curly braces and the return statement (implicit)
-      <div className="App">
-        { list.map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )}
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
 }
+
+// Converting an ES6 class component to a functional stateless component:
+// 1. Replace "class ClassName extends Component" with "function ClassName(props)"
+// 2. Best practice - destructure the props in the function signature
+// 3. Remove "render()"
+// 4. Replace "this.props" with "props" if any
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+const Table = ({ list, pattern, onDismiss }) => {
+  const largeColumn = { width: "40%" };
+  const midColumn = { width: "30%" };
+  const smallColumn = { width: "10%" };
+
+  return (
+    <div className="table">
+      { list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={midColumn}>
+            {item.author}
+          </span>
+          <span style={smallColumn}>
+            {item.num_comments}
+          </span>
+          <span style={smallColumn}>
+            {item.points}
+          </span>
+          <span style={smallColumn}>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+// Using default parameter to indicate className is optional
+const Button = ({ onClick, className = "", children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
