7591b43a0a0ccf12b495c82edd092cbc152d83d7
Waihon Yew <yewwaihon@gmail.com>
2017-03-23 01:15:46 +0800

    Getting real with an API

--- src/App.js
+++ src/App.js
@@ -1,72 +1,163 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
+// Fetching data
+const DEFAULT_QUERY = "redux";
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = "10";
 
-// Higher order function - A function which returns a function
-const isSearched = (searchTerm) => (item) =>
-  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+// https://hn.algolia.com/api/v1/search?query=redux&page=0&hitsPerPage=10
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage="
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // Shorthand property syntax - when the state and var have the same name
-      list,
-      searchTerm: ""
+      results: null,
+      searchKey: "", // key to refer cache of previous results
+      searchTerm: DEFAULT_QUERY
     };
 
-    this.onDismiss = this.onDismiss.bind(this);
+    // Binding of class methods
+    this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetSearchTopstories = this.fetchSearchTopstories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  needsToSearchTopstories(searchTerm) {
+    // Needs to search when cache is not found for the search term
+    return !this.state.results[searchTerm];
+  }
+
+  setSearchTopstories(result) {
+    // result is an object returned by the API which contains objectID,
+    // hits (array), page, hitsPerPage, nbHits, and so on.
+    const { hits, page } = result;
+    // results contains previous results cached by searchKey
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    // If the searchKey already exists in results then the existing content
+    // will be refreshed with the latest content.
+    this.setState({
+      results: {
+        ...results,
+        // [searchKey] is a computed property name.
+        // page is is shorthand property. This syntax can be used when
+        // both the property and value have the same name.
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  fetchSearchTopstories(searchTerm, page) {
+    // https://hn.algolia.com/api/v1/search?query=redux&page=0&hitsPerPage=10
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      // The response needs to get transformed to JSON, which is a
+      // mandatory step in a native fetch.
+      // The names response and result are arbitrary. They could be named as
+      // res and rst also.
+      .then(response => response.json())
+      // result contains the content returned by response.json()
+      .then(result => this.setSearchTopstories(result));
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
   }
 
   onDismiss(id) {
+    const { searchKey, results } = this.state
+    const { hits, page } = results[searchKey];
+
+    // This filtering function includes those items NOT having the
+    // desired ID.
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList })
+    const updatedHits = hits.filter(isNotId);
+
+    // If the searchKey already exists in results then the existing content
+    // will be refreshed with the latest content.
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
   }
 
+  // Syntatic event
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
 
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+
+    if (this.needsToSearchTopstories(searchTerm)) {
+      this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+    }
+
+    // Prevent refresh of web page
+    event.preventDefault();
+  }
+
   render() {
     // ES6 destructuring
-    const { searchTerm, list } = this.state;
+    const { searchTerm, results, searchKey } = this.state;
+
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
     return (
-      // A concise body wihout curly braces and the return statement (implicit)
       <div className="page">
         <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
         </div>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { results &&
+          <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopstories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
@@ -76,24 +167,27 @@ class App extends Component {
 // 1. Replace "class ClassName extends Component" with "function ClassName(props)"
 // 2. Best practice - destructure the props in the function signature
 // 3. Remove "render()"
-// 4. Replace "this.props" with "props" if any
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children} <input
+const Search = ({ value, onChange, onSubmit, children }) =>
+  // A concise body wihout curly braces nor return statement (implicit)
+  <form onSubmit={onSubmit}>
+    <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
 
-const Table = ({ list, pattern, onDismiss }) => {
+const Table = ({ list, onDismiss }) => {
   const largeColumn = { width: "40%" };
   const midColumn = { width: "30%" };
   const smallColumn = { width: "10%" };
 
   return (
     <div className="table">
-      { list.filter(isSearched(pattern)).map(item =>
+      { list.map(item =>
         <div key={item.objectID} className="table-row">
           <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
